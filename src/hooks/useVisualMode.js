481ace72281a910b27fda582abed6000fc36b7d4
MacKenzie Joyal <mackenzie.joyal@gmail.com>
2020-10-29 20:55:56 -0700

    added custom hook for managing modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,10 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 export default function useVisualMode (initial) {
   const [mode, setMode] = useState(initial);
-  function transition (newMode) {
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode, replace = false) {
+    if(replace) {
+      history.pop();
+    }
     setMode(newMode);
+    setHistory([...history, newMode]);
   }
-  
-  return {mode, transition};
+
+  function back() {
+    if(history.length > 1) {
+      setMode(history[history.length - 2])
+      history.pop();
+    }
+    console.log(history);
+  }
+ 
+  return {mode, transition, back};
 };
