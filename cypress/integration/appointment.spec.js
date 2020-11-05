e477784fcb6587dacf56bb3c09d0efa3ec64a6ab
MacKenzie Joyal <mackenzie.joyal@gmail.com>
2020-11-05 14:58:16 -0800

    added cy test for editing appointment

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -8,24 +8,27 @@ describe('Appointments', () => {
     cy.contains('Monday');
   })
 
-  // it('should book an interview', () => {
-  //   cy.get('.appointment__add-button')
-  //     .first()
-  //     .click() 
-  //   cy.get("[data-testid=student-name-input]")
-  //     .type('Mackenzie Joyal', {delay: 100})
-    
-  //   cy.get(".interviewers__item-image")
-  //     .first()
-  //     .click()
-  
-  //   cy.get('.button--confirm')
-  //     .click()
-    
-  //   cy.contains('.appointment__card--show', "Mackenzie Joyal")
+  afterEach(() => {
+    cy.wait(1000);
+  })
 
-  // });
+  it('should book an interview', () => {
+    cy.get('.appointment__add-button')
+      .first()
+      .click() 
+    cy.get("[data-testid=student-name-input]")
+      .type('Mackenzie Joyal', {delay: 100})
+    
+    cy.get(".interviewers__item-image")
+      .first()
+      .click()
   
+    cy.get('.button--confirm')
+      .click()
+    
+    cy.contains('.appointment__card--show', "Mackenzie Joyal")
+
+  });
   it('should edit an interview', () => {
     cy.get('.appointment__card')
       .first()
@@ -48,7 +51,15 @@ describe('Appointments', () => {
     cy.get('[alt="Delete"]')
       .invoke('show')
       .click()
-    cy.contains('Con')
+    cy.contains('Confirm')
+      .click()
+    cy.get('.schedule')
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Andrews")
+    .should("not.exist");
   });
   
 });
