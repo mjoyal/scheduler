b7a010c5a3521e585789aea386cfb7ba4be1332e
MacKenzie Joyal <mackenzie.joyal@gmail.com>
2020-11-05 14:48:27 -0800

    added cy test for deleting appointment

--- cypress/integration/appointment.spec.js
+++ cypress/integration/appointment.spec.js
@@ -8,23 +8,47 @@ describe('Appointments', () => {
     cy.contains('Monday');
   })
 
-  it('should book an interview', () => {
-    cy.get('.appointment__add-button')
-      .first()
-      .click() 
-    cy.get("[data-testid=student-name-input]")
-      .type('Mackenzie Joyal', {delay: 100})
+  // it('should book an interview', () => {
+  //   cy.get('.appointment__add-button')
+  //     .first()
+  //     .click() 
+  //   cy.get("[data-testid=student-name-input]")
+  //     .type('Mackenzie Joyal', {delay: 100})
     
-    cy.get(".interviewers__item-image")
-      .first()
-      .click()
+  //   cy.get(".interviewers__item-image")
+  //     .first()
+  //     .click()
   
-    cy.get('.button--confirm')
-      .click()
+  //   cy.get('.button--confirm')
+  //     .click()
     
-    cy.contains('.appointment__card--show', "Mackenzie Joyal")
+  //   cy.contains('.appointment__card--show', "Mackenzie Joyal")
 
+  // });
+  
+  it('should edit an interview', () => {
+    cy.get('.appointment__card')
+      .first()
+      .trigger('mouseover')
+      cy.get('[alt="Edit"]')
+        .invoke('show')
+        .click()
+      cy.get("[data-testid=student-name-input]")
+        .type("{backspace}{backspace}{backspace}{backspace}{backspace}{backspace} Andrews", {delay: 100})
+      
+        cy.get('.button--confirm')
+        .click()
+        cy.contains('.appointment__card--show', "Archie Andrews")
   });
 
+  it('should cancel an interview', () => {
+    cy.get('.appointment__card')
+    .first()
+    .trigger('mouseover')
+    cy.get('[alt="Delete"]')
+      .invoke('show')
+      .click()
+    cy.contains('Con')
+  });
   
 });
